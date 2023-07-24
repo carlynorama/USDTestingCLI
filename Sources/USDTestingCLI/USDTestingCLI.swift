bb76c609af664e7d321a826ada482e481290852e
carlynorama <carlynorama@gmail.com>
2023-07-23 19:23:44 -0700

    trying to do a tmp file.

--- Sources/USDTestingCLI/USDTestingCLI.swift
+++ Sources/USDTestingCLI/USDTestingCLI.swift
@@ -13,19 +13,20 @@ public struct USDTestingCLI:ParsableCommand {
         abstract: "A Swift command-line tool",
         version: "0.0.1",
         subcommands: [
-            testusdcat.self,
+            //testusdcat.self,
             checkncrate.self,
-            helloworld.self
+            helloworld.self,
+            teststring.self
         ],
-        defaultSubcommand: helloworld.self)
+        defaultSubcommand: teststring.self)
     
     public init() {}
     
-    struct testusdcat:ParsableCommand {
-        func run() throws {
-            print(USDServiceProvider(pathToUSDBuild:USDBuild, pythonEnv: pythonEnv).usdcatHelp())
-        }
-    }
+//    struct testusdcat:ParsableCommand {
+//        func run() throws {
+//            print(USDServiceProvider(pathToUSDBuild:USDBuild, pythonEnv: pythonEnv).usdcatHelp())
+//        }
+//    }
     
     struct checkncrate:ParsableCommand {
         
@@ -41,7 +42,7 @@ public struct USDTestingCLI:ParsableCommand {
             
             print("hello")
             
-            let reuslt = usdSP.check(inputFile)
+            let reuslt = usdSP.check(filePath: inputFile)
             print(reuslt)
             usdSP.makeCrate(from: inputFile, outputFile: outputFilePath)
         }
@@ -58,6 +59,103 @@ public struct USDTestingCLI:ParsableCommand {
             let usdSP = USDServiceProvider(pathToUSDBuild: USDBuild, pythonEnv: pythonEnv)
             usdSP.saveHelloWorld(to: outputFilePath)
         }
+    }
+    
+    struct teststring:ParsableCommand {
+        
+        func run() {
+            let usdSP = USDServiceProvider(pathToUSDBuild: USDBuild, pythonEnv: pythonEnv)
+            let result = usdSP.check(string: mockdata)
+            print(result)
+        }
+        
+        
+        var mockdata:String = """
+#usda 1.0
+(
+    startTimeCode = 1
+    endTimeCode = 10
+    upAxis = "Z"
+)
+def SkelRoot "Model"(
+    prepend apiSchemas = ["SkelBindingAPI"]
+)
+{
+    def Skeleton "Skel"(
+        prepend apiSchemas = ["SkelBindingAPI"]
+    )
+    {
+        uniform token[] joints = ["Shoulder", "Shoulder/Elbow", "Shoulder/Elbow/Hand"]
+        uniform matrix4d[] bindTransforms = [
+            ((1,0,0,0),(0,1,0,0),(0,0,1,0),(0,0,0,1)),
+            ((1,0,0,0),(0,1,0,0),(0,0,1,0),(0,0,2,1)),
+            ((1,0,0,0),(0,1,0,0),(0,0,1,0),(0,0,4,1))
+        ]
+        uniform matrix4d[] restTransforms = [
+            ((1,0,0,0),(0,1,0,0),(0,0,1,0),(0,0,0,1)),
+            ((1,0,0,0),(0,1,0,0),(0,0,1,0),(0,0,2,1)),
+            ((1,0,0,0),(0,1,0,0),(0,0,1,0),(0,0,2,1))
+        ]
+        def SkelAnimation "Anim1" {
+            uniform token[] joints = ["Shoulder/Elbow"]
+        
+            float3[] translations = [(0,0,2)]
+            quatf[] rotations.timeSamples = {
+                1: [(1,0,0,0)],
+                10: [(0.7071, 0.7071, 0, 0)]
+            }
+            half3[] scales = [(1,1,1)]
+        }
+        
+        rel skel:animationSource = <Anim1>
+    }
+    rel skel:skeleton = </Model/Skel>
+
+    def Mesh "Arm" (
+        prepend apiSchemas = ["SkelBindingAPI"]
+    )
+    {
+ 
+       int[] faceVertexCounts = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+       int[] faceVertexIndices = [
+           2, 3, 1, 0,
+           6, 7, 5, 4,
+           8, 9, 7, 6,
+           3, 2, 9, 8,
+           10, 11, 4, 5,
+           0, 1, 11, 10,
+           7, 9, 10, 5,
+           9, 2, 0, 10,
+           3, 8, 11, 1,
+           8, 6, 4, 11
+       ]
+       point3f[] points = [
+           (0.5, -0.5, 4), (-0.5, -0.5, 4), (0.5, 0.5, 4), (-0.5, 0.5, 4),
+           (-0.5, -0.5, 0), (0.5, -0.5, 0), (-0.5, 0.5, 0), (0.5, 0.5, 0),
+           (-0.5, 0.5, 2), (0.5, 0.5, 2), (0.5, -0.5, 2), (-0.5, -0.5, 2)
+       ]
+       # Authored normals should be ignored on skinned meshes.
+       normal3f[] normals = [
+           (0,0,1), (0,0,1), (0,0,1), (0,0,1),
+           (0,0,1), (0,0,1), (0,0,1), (0,0,1),
+           (0,0,1), (0,0,1), (0,0,1), (0,0,1)
+       ]
+       int[] primvars:skel:jointIndices = [
+           2,2,2,2, 0,0,0,0, 1,1,1,1
+       ] (
+           interpolation = "vertex"
+           elementSize = 1
+       )
+       float[] primvars:skel:jointWeights = [
+           1,1,1,1, 1,1,1,1, 1,1,1,1
+        ] (
+           interpolation = "vertex"
+           elementSize = 1
+       )
+       matrix4d primvars:skel:geomBindTransform = ((1,0,0,0),(0,1,0,0),(0,0,1,0),(0,0,0,1))
+    }
+}
+"""
     }
     
     
